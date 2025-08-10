23ac66969a3f94f36ba1b592ecb6c2a71a7b199e
Mrinmoy Adhikary <63409502+mrinmoy96@users.noreply.github.com>
2025-08-10 18:47:18 +0530

    Time: 0 ms (100%), Space: 10.3 MB (9.33%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -12,3 +12,47 @@ public:
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int n=matrix.size();
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 swap(matrix[i][j], matrix[j][i]);
+//             }
+//         }
+//         for(int i=0; i<n; i++){
+//             reverse(matrix[i].begin(), matrix[i].end());
+//         }
