d2e9658db708af35c1c0e8f83afc699ce42327dc
Mrinmoy Adhikary <63409502+mrinmoy96@users.noreply.github.com>
2026-01-06 23:50:19 +0530

    Time: 0 ms (100%), Space: 95.2 MB (43.92%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    /*
-    double median(vector<int>& ans){
-        double medi;
-        double sum=0;
-        for(int i=0; i<ans.size(); i++){
-            sum+=ans[i];
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-        medi=sum/ans.size();
-        return medi;
-    }
-    */
-    
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>nums2){
-        vector<int>v;
-    for(int i=0; i<nums1.size(); i++){
-        v.push_back(nums1[i]);
-    }
-    for(int i=0; i<nums2.size(); i++){
-        v.push_back(nums2[i]);
-    }
-    sort(v.begin(), v.end());
-   
-    int n=v.size();
-    if (n % 2 != 0){
-        return (double)v[n / 2];
-    }
-    return (double)(v[(n - 1) / 2] + v[n / 2]) / 2.0;
-    }
         
-    
+        int m = nums1.size();
+        int n = nums2.size();
+        int low = 0, high = m;
+        
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+            
+            // Edge cases: if partition is at the very beginning or end
+            int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
+            
+            int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int minRightY = (partitionY == n) ? INT_MAX : nums2[partitionY];
+            
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                // We have found the correct partition
+                if ((m + n) % 2 == 0) {
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                // We are too far right in nums1, move left
+                high = partitionX - 1;
+            } else {
+                // We are too far left in nums1, move right
+                low = partitionX + 1;
+            }
+        }
+        
+        return 0.0;
+    }
 };
