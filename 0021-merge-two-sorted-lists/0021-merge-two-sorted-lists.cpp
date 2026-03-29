d392228ffc88648b93f78de41a48a7068dfad34b
Mrinmoy Adhikary <63409502+mrinmoy96@users.noreply.github.com>
2026-03-29 12:22:26 +0530

    Time: 0 ms (100%), Space: 19.5 MB (61.85%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,58 +11,24 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL && list2==NULL){
-            return NULL;
-        }
-        if(list1==NULL){
-            return list2;
-        }
-        if(list2==NULL){
-            return list1;
-        }
-        ListNode* fh=NULL;
-        ListNode* ft=NULL;
+       ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
 
-        if(list1->val < list2->val){
-            fh=list1;
-            ft=list1;
-            list1=list1->next;
-        }
-        else{
-            fh=list2;
-            ft=list2;
-            list2=list2->next;
-        }
-
-        while(list1!=NULL && list2!=NULL){
-            if(list1->val< list2->val){
-                ft->next=list1;
-                ft=ft->next;
-                list1=list1->next;
+        while (list1 && list2) {
+            if (list1->val > list2->val) {
+                cur->next = list2;
+                list2 = list2->next;
+            } else {
+                cur->next = list1;
+                list1 = list1->next;
             }
-            else{
-                ft->next=list2;
-                ft=ft->next;
-                list2=list2->next;
-            }
-        }
-        while(list1!=NULL){
-            ft->next=list1;
-            ft=ft->next;
-            list1=list1->next;
-        }
-        while(list2!=NULL){
-            ft->next=list2;
-            ft=ft->next;
-            list2=list2->next;
+            cur = cur->next;
         }
 
-        return fh;
+        cur->next = list1 ? list1 : list2;
+
+        ListNode* head = dummy->next;
+        delete dummy;
+        return head;     
     }
 };
-
-
-
-
-
-
